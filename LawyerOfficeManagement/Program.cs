56285ff4d3a0fbfafc0b9754f5fe0d8b1f7789cd
Bruno Ferreira <bruno.ferreira.rocha@gmail.com>
2022-05-03 14:16:55 +0200

    Aggiungere i file di progetto.

---
+++ LawyerOfficeManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LawyerOfficeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
